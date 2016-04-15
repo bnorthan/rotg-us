7dfcdad17f027d0c6b990acff5541e081a327ae8
Brian Northan <bnorthan@gmail.com>
2016-04-15 16:58:01 -0400

    sortable tables

--- site/runnin/results/views.py
+++ site/runnin/results/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Member
 from .models import Race
+from .models import Result
+
 from django.shortcuts import get_object_or_404, render
 
 import django_tables2 as tables
@@ -29,17 +31,76 @@ def races(request):
 
 
 class SimpleRace(tables.Table):
+    class Meta:    
+        #model=Result
+        attrs={"class":"paleblue"}
+  
     first_name=tables.Column()
     last_name=tables.Column()
-    time=tables.Column()
+    
+    gender=tables.Column()
+    age=tables.Column()
+    city=tables.Column()
+    state=tables.Column()
+    
+    split1=tables.Column()
+    splits=tables.Column()
 
+    time=tables.Column()
+    
     def render_time(self, value):
         return value.strftime('%M:%S')
 
-def race(request, race_id):
-    race=SimpleRace(Race.objects.get(pk=race_id).result_set.all())
-    #race=Race.objects.get(pk=race_id).result_set.all()
+'''
+    testles=tables.Column(empty_values=())
 
+    def render_testles(self, value):
+        return 4
+'''
+def simple_with_splits():
+
+    def render_split(self, value, record):
+        print splits
+        if (record.splits==None): return 'Na'
+       
+        return record.splits.split(',')[0]
+
+    
+
+    attrs={"testbest":tables.Column(empty_values=()),"class":"paleblue"}
+   
+    attrs['Meta'] = type('Meta', (), dict(attrs={"class":"paleblue", "orderable":"True", "width":"100%"}))  
+    attrs['render_testbest']=render_split
+    
+    klass=type('SimpleWithSplits',(SimpleRace,),attrs)
+    return klass
+
+from django.forms.models import model_to_dict
+
+def race(request, race_id):
+    race=[]
+
+    results=Race.objects.get(pk=race_id).result_set.all()
+
+    for r in results:
+        #temp={}
+        temp=model_to_dict(r)
+        temp['name']=r.first_name
+        splits=r.splits
+
+        if (splits!=None):
+            temp['split1']=splits.split(',')[0]
+        else:
+            temp['split1']='Na'
+        race.append(temp)
+
+
+    
+    #race=SimpleRace(Race.objects.get(pk=race_id).result_set.all())
+    #race=Race.objects.get(pk=race_id).result_set.all()
+    #race=simple_with_splits()(Race.objects.get(pk=race_id).result_set.all())
+    race=SimpleRace(race)
+    
     RequestConfig(request).configure(race)
     
     return render(request, 'race/index.html', {'l_race':race})
